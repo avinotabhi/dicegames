f9a0b20ed3edbea9cfc1baaecdff9ec4168847f8
avinotabhi <118135667+avinotabhi@users.noreply.github.com>
2023-02-03 17:29:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //dice 1
 var randomnumber1 = Math.floor(Math.random()*6) +1 ; 
-var diceimage = "images/dice" + randomnumber1 + ".png" ; 
+var diceimage = "dice" + randomnumber1 + ".png" ; 
 var finalimg = document.querySelectorAll("img")[0]; 
 finalimg.setAttribute("src",diceimage); 
 //dice 2
 var randomnumber2 = Math.floor(Math.random()*6) +1 ; 
-var diceimage2 = "images/dice" + randomnumber2 + ".png" ; 
+var diceimage2 = "dice" + randomnumber2 + ".png" ; 
 var finalimg2 = document.querySelectorAll("img")[1]; 
 finalimg2.setAttribute("src",diceimage2); 
 
